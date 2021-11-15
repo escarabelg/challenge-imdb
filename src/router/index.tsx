236e8f216d2a42d5b2e5090d52836bdfcbfbb705
Guilherme Escarabel <escarabelg@gmail.com>
2021-11-15 14:37:53 -0400

    feat(router): add reference to pages/movie

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,12 +3,14 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 // components
 import { Home } from '../pages/Home'
+import { Movie } from '../pages/Movie'
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Movie />} />
       </Routes>
     </BrowserRouter>
   )
