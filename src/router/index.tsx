4a2d668a48c822b87cc674072e6cc8cdc937c91f
Guilherme Escarabel <escarabelg@gmail.com>
2021-11-15 16:58:12 -0400

    feat(router): upgrade code to v6

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { useRoutes } from 'react-router'
 
 // components
 import { Home } from '../pages/Home'
 import { Movie } from '../pages/Movie'
 
 export function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Movie />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: ":id", element: <Movie /> },
+  ])
+  return routes;
 }
 
